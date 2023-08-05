2fc9f0a7dab1a2f41032cfa8d70202c9100f5d02
Nahum Martinez <111940941+NahumMartinez01@users.noreply.github.com>
2023-08-04 18:52:07 -0600

    finalización calculadora

--- app.js
+++ app.js
@@ -80,7 +80,7 @@ function calculate() {
 }
 
 function add(num1, num2) {
-  console.log(num1 + num2);
+  
   return num1 + num2;
 }
 
@@ -94,7 +94,7 @@ function multiply(num1, num2) {
 
 function divide(num1, num2) {
   if (num2 === 0) {
-    return null;
+    return 0;
   } else {
     return num1 / num2;
   }
